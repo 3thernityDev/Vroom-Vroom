e7696cf077151c37d9b8bc06077977f3f0e537ff
3thernitydev <3thernitydev@gmail.com>
2025-01-14 10:37:36 +0400

    add factoryFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CarFactory;
+use App\Factory\MotoFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        CarFactory::createMany(10);
+        MotoFactory::createMany(10);
 
         $manager->flush();
     }
